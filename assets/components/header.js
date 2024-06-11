b6224687149556f7408d288728d211fc8493c2dc
Christiaan Botha <64542429+Toast1e@users.noreply.github.com>
2024-06-11 13:56:25 +0200

    Add files via upload

---
+++ assets/components/header.js
@@ -0,0 +1,15 @@
+<header>
+  <nav>
+    <ul>
+      <li>
+        <a href="/index.html">Home</a>
+      </li>
+      <li>
+        <a href="assets/pages/misc/about.html">About</a>
+      </li>
+      <li>
+        <a href="contact.html">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>;
